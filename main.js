aca94d02075c8f57500a6f480a441b28f6209de9
Snowhite3105 <83868245+Snowhite3105@users.noreply.github.com>
2022-09-11 18:01:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 function setup(){
-  canvas = createCanvas (350, 350);
-  canvas.center;
+    Canvas = createCanvas(350,350);
+    Canvas.center();
   video = createCapture(VIDEO);
-  video.size = 350, 350;
+  video.size(350, 350);
   video.hide();
-poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet = ml5.poseNet(video, modelLoaded);
   poseNet.on('pose', gotPoses);
 }
 function draw(){
     image(video, 0, 0, 350, 350 );
 }
-function saveImage(){
-  save("Lipstick_Filter_Image");
+function saveimage(){
+    save("FIltered_image.png");
 }
-  
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
+}
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("lipstick_x = " + results[0].pose.nose.x);
+        console.log("lipstick_y = " + results[0].pose.nose.y);
+    }
+}
