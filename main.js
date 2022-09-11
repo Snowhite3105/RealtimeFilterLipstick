83d0bb54299773a18a1c563cfe7037a46fd010ff
Snowhite3105 <83868245+Snowhite3105@users.noreply.github.com>
2022-09-11 10:29:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-
+function setup(){
+  canvas = createCanvas (350, 350);
+  canvas.center;
+  video = createCapture(VIDEO);
+  video.size = 350, 350;
+  video.hide();
+poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+function draw(){
+    image(video, 0, 0, 350, 350 );
+}
+function saveImage(){
+  save("Lipstick_Filter_Image");
+}
+  
